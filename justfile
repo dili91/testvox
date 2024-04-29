38a8bad2e395f91beede69e8db2a3e11c4999b36
Andrea Di Lisio <andrea.dilisio91@gmail.com>
2024-04-29 16:12:24 +0200

    test

--- justfile
+++ justfile
@@ -4,17 +4,19 @@ alias b     := build
 alias r     := run
 alias t     := test
 
+default_test_reports_pattern := "./test-reports/**/*.xml"
+
 docker-build:
     docker build . -t reportly
 
 docker-run:
-    @docker run --rm -v "./test-reports/basic:/home/reportly/test-reports/" reportly
+    docker run --rm -v "./test-reports:/home/reportly/test-reports" reportly reportly "{{default_test_reports_pattern}}"
 
 build:
     cargo build
 
-run:
-    cargo run
+run test_reports_pattern=default_test_reports_pattern:
+    cargo run -- "{{test_reports_pattern}}"
 
 test: 
     cargo nextest run
--- src/main.rs
+++ src/main.rs
@@ -8,27 +8,29 @@ use reportly::{
 
 //TODO: remove code that panics
 fn main() {
-    let slack_report: SlackReport = build_reports("./test-reports/**/*.xml")
+    let test_reports_pattern = std::env::args().nth(1).expect("no test-reports pattern given");
+
+    let report: SlackReport = build_reports(&test_reports_pattern)
         .expect("Unable to create test report")
         .into();
 
     println!(
         "{}",
-        serde_json::to_string_pretty(&slack_report).expect("unable to serialize to JSON string")
+        serde_json::to_string_pretty(&report).expect("unable to serialize to JSON string")
     )
 }
 
 // TODO: use a builder
 fn build_reports(junit_reports_file_pattern: &str) -> Result<Vec<TestReport>> {
     let mut reports: Vec<TestReport> = vec![];
-    for test_file in glob(junit_reports_file_pattern)? {
+    for test_file in glob(junit_reports_file_pattern).expect("something went wrong") {
         match test_file {
             Ok(path) => {
                 let junit_parser = JunitTestParser {
                     file_name: path.to_str().unwrap().to_string(),
                 };
                 let report: TestReport = junit_parser.parse()?;
-                reports.push(report)
+                reports.push(report);
             }
             Err(e) => println!("{:?}", e),
         }
