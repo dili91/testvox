cd00c6d66df7e5b73f7660a635df9676c126bba5
Andrea Di Lisio <andrea.dilisio91@gmail.com>
2024-04-30 16:09:24 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 # This entrypoint is only use by Github actions 
 
 echo 'REPORT<<EOF' >> $GITHUB_OUTPUT
-reportly --report-title $1 --test-reports-pattern $2 >> $GITHUB_OUTPUT
+reportly --report-title "$1" --test-reports-pattern "$2" >> $GITHUB_OUTPUT
 echo 'EOF' >> $GITHUB_OUTPUT
