9ab9bf88e18f8d73706999f19ea8f1ea715cec36
Andrea Di Lisio <andrea.dilisio91@gmail.com>
2024-05-02 12:58:02 +0200

    refactor main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use clap::Parser;
 use glob::glob;
 use reportly::{
@@ -20,19 +22,27 @@ fn main() {
     // Parse CLI arguments
     let args = CliArgs::parse();
 
-    // Parse test results
-    let junit_test_results: Vec<TestResult> = glob(&args.test_reports_pattern)
+    let test_results_files = glob(&args.test_reports_pattern)
         .expect("Unable to use given file pattern")
         .filter_map(|test_file| test_file.ok())
+        .collect::<Vec<PathBuf>>();
+
+    if test_results_files.is_empty() {
+        eprintln!("Cannot find test results file");
+        return;
+    }
+
+    let test_results: Vec<TestResult> = test_results_files
+        .into_iter()
         .map(|test_file| JunitTestParser::new(test_file).parse())
         .filter_map(|test_results| test_results.ok())
         .flatten()
         .collect();
 
-    // Build the final report
+    // Build and print the final report
     let report = SlackReport::builder()
         .with_title(args.report_title)
-        .with_test_blocks(junit_test_results)
+        .with_test_blocks(test_results)
         .build();
 
     println!(
